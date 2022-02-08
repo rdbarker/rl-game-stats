90a0a905647591e6026a146f486cc1b3d80cdaca
rdbarker <ryan.dean.barker@gmail.com>
2022-02-07 22:57:29 -0500

    apikey text input with localstorage

---
+++ src/components/KeyInput.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let apiKey = "";
+
+    if (localStorage.getItem("ballchasing")){
+        apiKey = localStorage.getItem("ballchasing");
+    }
+
+    $:localStorage.setItem("ballchasing", apiKey);
+</script>
+
+<input bind:value={apiKey} placeholder="ballchasing api key"> 
