82f69f5471eae1b061077d98fbf08b7da8ad3c98
rdbarker <ryan.dean.barker@gmail.com>
2022-02-12 22:05:36 -0500

    single match component

---
+++ src/components/Match.svelte
@@ -0,0 +1,21 @@
+<script>
+    import TimeHeader from "./match/TimeHeader.svelte";
+
+    export let gameData = {};
+
+</script>
+
+<style>
+    div{
+        width: 1200px;
+        height: 120px;
+        border: 2px solid darkslategrey;
+    }
+</style>
+
+<div>
+    <h3>{gameData.team_size} vs {gameData.team_size}</h3>
+    <h4><TimeHeader timeString={gameData.date}/></h4>
+</div>
+
+
