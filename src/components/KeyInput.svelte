178015a87354e0a56eeaf3067d08b640e8165c5a
rdbarker <ryan.dean.barker@gmail.com>
2022-02-12 22:05:51 -0500

    input key text box component

--- src/components/KeyInput.svelte
+++ src/components/KeyInput.svelte
@@ -1,11 +1,31 @@
 <script>
+    import {fetchPing} from "./scripts/apiFetch"
+
     export let apiKey = "";
+    export let keyVerified = false;
+
+    let verifiedJson = {};
+    let iconStatus = {
+        true:{"src":"icon-checkmark", "color":"#74d978"}, 
+        false:{"src":"icon-x", "color":"#e75c5c"},
+        undefined: {"src":"icon-x", "color":"#e75c5c"}
+    }
 
     if (localStorage.getItem("ballchasing")){
         apiKey = localStorage.getItem("ballchasing");
     }
+    $: localStorage.setItem("ballchasing", apiKey);
 
-    $:localStorage.setItem("ballchasing", apiKey);
+    let promise = fetchPing(apiKey).then(
+        value => {
+                    verifiedJson = value;
+                    keyVerified = verifiedJson["chaser"] ? true : false;
+                }
+    
+    );
+
 </script>
-
-<input bind:value={apiKey} placeholder="ballchasing api key"> 
+<div>
+    <input bind:value={apiKey} placeholder="ballchasing api key"> 
+    <span class="{iconStatus[keyVerified]["src"]}" style="color:{iconStatus[keyVerified]["color"]}"> </span>
+</div>
