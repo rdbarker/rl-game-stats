56eba39d3781cee2b8f217d5d029b2b116dde9e8
rdbarker <ryan.dean.barker@gmail.com>
2022-02-13 12:22:23 -0500

    refresh match one at a time

--- src/components/MatchesList.svelte
+++ src/components/MatchesList.svelte
@@ -1,5 +1,6 @@
 <script>
-    import {fetchListOfGames} from "./scripts/apiFetch";
+    import {fetchGame, fetchListOfGames} from "./scripts/apiFetch";
+    import Match from "./Match.svelte"
     export let apiKey = "";
     export let keyVerified = false;
 
@@ -7,9 +8,11 @@
 
 	let time = new Date();
     let secondsBetweenRefresh = 10;
-    let games = [];
+    let games = new Map();
+    
 
 	$: seconds = time.getSeconds();
+    $: if(seconds % secondsBetweenRefresh == 0 && keyVerified) refreshGames();
 
 	onMount(() => {
 		const interval = setInterval(() => {
@@ -20,12 +23,33 @@
 			clearInterval(interval);
 		};
 	});
+
     let gamesPromise = fetchListOfGames(apiKey, 15);
-    async function refresh(){
-        if(seconds % secondsBetweenRefresh == 0 && keyVerified){
-            gamesPromise = await fetchListOfGames(apiKey, 15);
-            console.log(gamesPromise);
+
+    async function refreshGames(){
+        gamesPromise = await fetchListOfGames(apiKey, 15);
+        gamesPromise.list.forEach(item =>{
+            if (!games.has(item.id)){
+                games.set(item.id, {}); 
+            }
+        });
+
+        for(const [key, value] of games){
+            if(value === false || !("status" in value)){
+                games.set(key, await loadMatch(key));
+            } 
+        }
+        games = games;
     }
-}
+
+    async function loadMatch(gameId){
+        return await fetchGame(apiKey, gameId);
+    }
+
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
 </script>
 
+{#each [...games] as [gameId, stats]}
+    <Match gameData = {stats}/>
+{/each}
