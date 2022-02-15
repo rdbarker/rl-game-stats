d5f78603867fac4d0a5dd7be77638e817da193e7
rdbarker <ryan.dean.barker@gmail.com>
2022-02-15 18:10:01 -0500

    building match data need to display, early stages

--- src/components/Match.svelte
+++ src/components/Match.svelte
@@ -1,8 +1,42 @@
 <script>
     import TimeHeader from "./match/TimeHeader.svelte";
-    
+    import Player from "./match/Player.svelte";
+
     export let gameData = {};
+    let isDataLoaded = false;
+    let gameStats = {
+        time: "timestring",
+        map: "map_code",
+        matchType: "match_type",
+        teamSize: "team_size"
+    };
+    let myTeam = {
+  
+    };
+    let otherTeam = {
+        color: "blue",
+        players: {},
+
+    };
+
+    $: if(gameData.status === "ok"){
+        isDataLoaded = true;
+        buildGameStats();
+    }
+
+    function buildGameStats(){
+        let usersTeam = "blue";
+        //find user's team
+        let usersId = gameData.uploader.steam_id;
+        let teamBlue = gameData.blue;
+        let teamOrange = gameData.orange;
+
+        let isPlayerOrange = teamOrange.players.map(player =>player.id.id ).includes(usersId);
+        if (isPlayerOrange) usersTeam = "orange";
+    }
+
     
+
 </script>
 
 
@@ -13,13 +47,18 @@
         height: 120px;
         border: 2px solid darkslategrey;
         margin: 16px;
+        
     }
 </style>
 
 <div>
-   {#if gameData.status === "ok"}
+   {#if isDataLoaded}
         <TimeHeader timeString={gameData.date}/>
-        {:else}
+        
+        {#each gameData.blue.players as bluePlayer}
+            <Player name={bluePlayer.name} platform={bluePlayer.id.platform} platformId={bluePlayer.id.id}/>
+        {/each}
+    {:else}
         loading...
     {/if}
 </div>
