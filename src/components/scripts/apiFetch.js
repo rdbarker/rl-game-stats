a13679802b455525405f7d831983ffdaebb9170c
rdbarker <ryan.dean.barker@gmail.com>
2022-02-15 18:11:23 -0500

    fetch stats - wip

--- src/components/scripts/apiFetch.js
+++ src/components/scripts/apiFetch.js
@@ -1,16 +1,25 @@
 async function apiFetch(route, key, uris) {
-  const requestOptions = {
+  let requestOptions = {
     method: "GET",
     headers: { Authorization: key },
     mode: "cors",
   };
   let ballChasingRoute =
     "https://safe-plateau-49424.herokuapp.com/https://ballchasing.com/api/";
+  let statsRoute =
+    "https://api.tracker.gg/api/v2/rocket-league/standard/profile/steam/76561198031153890";
   let fetchRoute = ballChasingRoute;
   //check route and apend
+  if (route === "stats") {
+    fetchRoute = statsRoute;
+    requestOptions = {
+      method: "GET",
+      mode: "cors",
+    };
+  }
 
   //fetch json
-  fetchRoute += uris;
+  if (uris) fetchRoute += uris;
   let response = await fetch(fetchRoute, requestOptions);
   let text = await response.json();
   if (response.ok) {
@@ -34,4 +43,10 @@ async function fetchListOfGames(key, maxCount = 15, startDate) {
   return apiFetch("bc", key, uris);
 }
 
-export { fetchGame, fetchPing, fetchListOfGames };
+async function fetchStats() {
+  return apiFetch("stats");
+}
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+export { fetchGame, fetchPing, fetchListOfGames, fetchStats, delay };
