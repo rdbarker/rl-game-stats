ca47404852d33e896e3d3c583936ae3b7423a686
rdbarker <ryan.dean.barker@gmail.com>
2022-02-15 18:09:28 -0500

    changed to use stores minor clean up

--- src/components/KeyInput.svelte
+++ src/components/KeyInput.svelte
@@ -1,36 +1,36 @@
 <script>
-import { onMount } from "svelte";
+  import { fetchPing, delay } from "./scripts/apiFetch";
+  import { apiKey, keyVerified } from "./scripts/stores.js";
 
-    import {fetchPing} from "./scripts/apiFetch"
+  let iconStatus = {
+    true: { src: "icon-checkmark", color: "#74d978" },
+    false: { src: "icon-x", color: "#e75c5c" },
+    undefined: { src: "icon-x", color: "#e75c5c" },
+  };
 
-    export let apiKey = "";
-    export let keyVerified = false;
+  $: localStorage.setItem("ballchasing", $apiKey);
+  if (localStorage.getItem("ballchasing")) {
+    $apiKey = localStorage.getItem("ballchasing");
+  }
 
-    let verifiedJson = {};
-    let iconStatus = {
-        true:{"src":"icon-checkmark", "color":"#74d978"}, 
-        false:{"src":"icon-x", "color":"#e75c5c"},
-        undefined: {"src":"icon-x", "color":"#e75c5c"}
-    }
-
-    $: localStorage.setItem("ballchasing", apiKey);
-    if (localStorage.getItem("ballchasing")){
-            apiKey = localStorage.getItem("ballchasing");
-        }
-    onMount(() =>{
-
-            let promise = fetchPing(apiKey).then(
-                value => {
-                            verifiedJson = value;
-                            keyVerified = verifiedJson["chaser"] ? true : false;
-                        }
-            );
+  $: {
+    fetchPing($apiKey).then((json) => {
+      $keyVerified = json["chaser"];
+      delay(1000);
     });
-
-    
-
+  }
 </script>
+
 <div>
-    <input bind:value={apiKey} placeholder="ballchasing api key"> 
-    <span class="{iconStatus[keyVerified]["src"]}" style="color:{iconStatus[keyVerified]["color"]}"> </span>
+  <input bind:value={$apiKey} placeholder="ballchasing api key" />
+  <span
+    class={iconStatus[$keyVerified]["src"]}
+    style="color:{iconStatus[$keyVerified]['color']}"
+  />
 </div>
+
+<style>
+  input {
+    min-width: 24em;
+  }
+</style>
