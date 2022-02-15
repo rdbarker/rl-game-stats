e242a68d48dce7698378828c163dce2b72a19878
rdbarker <ryan.dean.barker@gmail.com>
2022-02-15 18:11:05 -0500

    player component - placeholder

---
+++ src/components/match/Player.svelte
@@ -0,0 +1,16 @@
+
+<script>
+    export let name;
+    export let platform;
+    export let platformId;
+</script>
+
+<div>
+    <h5>{name} | {platform} | {platformId}</h5>
+</div>
+
+<style>
+    h5{
+        margin:0;
+    }
+</style>
