8d43cc34bdab5802de5d335d1b331fc1296ef43b
rdbarker <ryan.dean.barker@gmail.com>
2022-03-23 19:42:14 -0400

    push

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,38 @@
 <script>
   import KeyInput from "./components/KeyInput.svelte";
   import MatchesList from "./components/MatchesList.svelte";
+  import axios from "axios";
+
+  async function getUser() {
+    try {
+      const response = await axios.get(
+        "https://api.tracker.gg/api/v2/rocket-league/standard/profile/xbl/Deadshot%20Datboi"
+      );
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function g() {
+    try {
+      const r = await fetch(
+        "https://api.tracker.gg/api/v2/rocket-league/standard/profile/steam/76561198031153890",
+        {
+          method: "GET",
+          mode: "no-cors",
+        }
+      );
+      console.log(r);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  getUser();
 </script>
 
-<div>
-  <KeyInput />
-  <MatchesList />
-</div>
+<div />
 
 <style>
   div {
